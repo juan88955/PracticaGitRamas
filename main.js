f2b1f9608eb8f114f9be6fee57ca7cabb020cb5b
Allpez <alexlop1987@gmail.com>
2024-08-17 11:41:45 -0500

    cuadrados crecen

--- main.js
+++ main.js
@@ -27,3 +27,29 @@ document.addEventListener('DOMContentLoaded', function() {
         return '#' + Math.floor(Math.random()*16777215).toString(16);
     }
 });
+
+function renderShapeDos(page) {
+    page.innerHTML = '';
+
+    for (let i = 0; i < 10; i++) {
+        const shape = document.createElement("div");
+        shape.className = "p-0 m-2 col-6 col-sm-4 col-md-2";
+        shape.innerHTML = `
+            <div class="square">
+            </div>`;
+
+        shape.addEventListener('mouseover', function () {
+            this.classList.add("squareGrow")
+        });
+        shape.addEventListener('mouseout', function () {
+            this.classList.remove("squareGrow")
+        });
+        page.appendChild(shape);
+    };
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const pageElement = document.getElementById('shapeDos');
+    renderShapeDos(pageElement);
+});
+
